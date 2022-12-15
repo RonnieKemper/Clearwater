3c77b0ba48dd78e227476072e2f1ee6c8b6e7b20
RonaldKemper <ronspoolco@gmail.com>
2022-12-14 21:34:19 -0500

    12-14-22 p2

--- clearwater_front/src/app/login/login.component.ts
+++ clearwater_front/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, Injectable, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,7 +16,31 @@ export class LoginComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
   getData(){
-    this.http.get('http://localhost:3000/users/profile')
+    return this.http.get('http://localhost:3000/users/profile')
+  }
+  login(){
+    console.log('login function initiated');
+    const headers = new HttpHeaders({
+      'myHeader': 'clearwater',
+      'Content-Type': 'application/json',
+      'Authorization': 'my-auth-token',
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    });
+    console.log('http request initiated');
+    var username = (<HTMLIonInputElement>document.getElementById('username')).value;
+    //var Firstname = (<HTMLInputElement>document.getElementById('Firstname')).value;
+    //var Lastname = (<HTMLInputElement>document.getElementById('Lastname')).value;
+    //var Email = (<HTMLInputElement>document.getElementById('Email')).value;
+    //var zip = (<HTMLInputElement>document.getElementById('zip')).value;
+    //var email = (<HTMLIonInputElement>document.getElementById('email')).value;
+    var password = (<HTMLIonInputElement>document.getElementById('password')).value;
+    var creds = [password, username];
+    //alert(creds);
+
+    this.http.post<{name: string}>('http://localhost:3000/users/login', creds, {headers: headers}).subscribe((res) => {
+      console.log('help');
+    });
   }
   ngOnInit() {}
 
--- clearwater_front/src/app/sign-up/sign-up.component.ts
+++ clearwater_front/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,8 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {}
 
-}
+  createProfile(){
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/json',
+    //   'Authorization': 'my-auth-token',
+    //   'Access-Control-Allow-Origin': 'http://localhost:4200',
+    //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    // });
+    
+    console.log('test');
+    
+    const headers = new HttpHeaders({
+      'myHeader': 'proacademy',
+      'Content-Type': 'application/json',
+      'Authorization': 'my-auth-token',
+      'Access-Control-Allow-Origin': 'http://localhost:4200',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+    });
+    var username = (<HTMLIonInputElement>document.getElementById('username')).value;
+    var Firstname = (<HTMLInputElement>document.getElementById('Firstname')).value;
+    var Lastname = (<HTMLInputElement>document.getElementById('Lastname')).value;
+    var Email = (<HTMLInputElement>document.getElementById('Email')).value;
+    //var zip = (<HTMLInputElement>document.getElementById('zip')).value;
+    //var email = (<HTMLIonInputElement>document.getElementById('email')).value;
+    var password = (<HTMLIonInputElement>document.getElementById('password')).value;
+    var creds = [Firstname, Lastname, Email, password, username];
+    //alert(creds);
+
+    // this.http.get('localhost:3000/users/login').subscribe((res) => {console.log(res)})
+    this.http.post<{name: string}>('http://localhost:3000/users/signup', creds, {headers: headers})  //products,
+      .subscribe((res) => {
+        console.log('help');
+      });
+    }
+
+  // createProfile(){
+  //   this.http.get('localhost:3000/users/signup')
+  //     .subscribe(data => {
+  //     // Handle the response data
+  //     alert(data)
+  // });
+  // }
+  }
+
+
